c54cf4d1d67c4f7dd66901916645ce3cd466aefb
Shiwi-1234 <shiwani@preciousinfosystem.com>
2023-01-11 19:34:00 +0530

    Initial commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_articles,only: %i[show , edit , update , destroy ]
+
   def index
     @articles = Article.all
   end
@@ -6,14 +8,18 @@ class ArticlesController < ApplicationController
   
   def new
     @article = Article.new
+
   end
 
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def edit
   end
 
   def create
     @article = Article.new(article_params)
+
       if @article.save
         redirect_to articles_path
       else
@@ -21,10 +27,28 @@ class ArticlesController < ApplicationController
       end   
   end
 
+  def update
+      if @article.update(article_params)
+        redirect_to @articles
+      else
+        render 'edit'
+      end 
+   end     
+
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 private
   
   def article_params
     params.require(:article).permit(:title, :description, :date, :day)
+  end
+
+  def set_articles
+      @article = Article.find(params[:id])
   end
 
 end
